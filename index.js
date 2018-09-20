7c2f4ed28bca7ee61f1bda8bf4967c584ad8beb0
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-09-19 23:53:58 -0700

    Add shell and base functions to build payload for prompts

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 const prompts = require('prompts');
-// {
-// 	title: ''
-// 	value: ''
-// }
-let response = await prompts(
-	{
-		type: 'multiselect',
-		name: 'osb-modules',
-		message: 'Select modules to deploy',
-		choices: [
-			{ title: 'Red', value: '#ff0000' },
-			{ title: 'Green', value: '#00ff00' },
-			{ title: 'Blue', value: '#0000ff', selected: true }
-		],
-		hint: '- Space to select. Return to submit'
-	}
-)
+const shell = require('shelljs');
 
-console.log(response);
+if (!shell.which('git')) {
+	shell.echo('Sorry, this script requires git');
+	shell.exit(1);
+}
+
+const cwd = shell.exec('pwd')
+
+/**
+ * Return array of directories containing build.gradle file
+ */
+const getDirectories = () => {
+	const shellResults = shell.exec('git ls-files | grep build.gradle')
+
+	let osbProjects = [];
+
+	shellResults.stdout.split('\n').map(
+		directory => {
+			const trimmedDir = directory.substring(0, directory.lastIndexOf("/"));
+
+			if (trimmedDir !== '') {
+				osbProjects.push(trimmedDir);
+			}
+		}
+	)
+
+	return osbProjects;
+}
+
+/**
+ * Accept array of file paths
+ * Return array of objects formatted for prompts
+ */
+const buildPayload = (directories) => {
+	let dirName;
+	let retVal = [];
+
+	directories.map(directory => {
+		dirName = directory.substring(directory.lastIndexOf("/") + 1)
+
+		retVal.push({title: dirName, value: directory})
+	})
+
+	return retVal;
+}
+
+const main = async () => {
+	const directories = getDirectories();
+	const payload = buildPayload(directories);
+
+	let response = await prompts(
+		{
+			type: 'multiselect',
+			name: 'osb-modules',
+			message: 'Select modules to deploy',
+			choices: payload,
+			hint: '- Space to select. Return to submit'
+		}
+	)
+
+	console.log(response);
+};
+
+main();
